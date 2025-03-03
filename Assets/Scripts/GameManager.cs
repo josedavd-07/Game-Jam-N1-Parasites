ff4e4c74ea2663e2b864a5404bd14ea3c049ec5c
AndrewBlackstar <dracokaisar@gmail.com>
2025-03-03 17:09:20 -0500

    uicontrol script, gameManager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,79 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System;
+
+public enum DifficultyLevel
+{
+    Easy,
+    Medium,
+    Hard
+}
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager GameInstance { get; private set; }
+    public bool isPaused;
+
+    public DifficultyLevel currentDifficult;
+
+    public event Action OnWin;
+    public event Action OnLose;
+
+    private void Awake()
+    {
+        if (GameInstance == null)
+        {
+            GameInstance = this;
+            DontDestroyOnLoad(this.gameObject);
+
+            currentDifficult = (DifficultyLevel)PlayerPrefs.GetInt("Difficulty", (int)DifficultyLevel.Easy);
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    public void GameOverWin()
+    {
+        Debug.Log("You win!");
+        isPaused = true;
+        Time.timeScale = 0;
+        OnWin?.Invoke();
+    }
+
+    public void GameOverLose()
+    {
+        Debug.Log("You lose!");
+        isPaused = true;
+        Time.timeScale = 0;
+        OnLose?.Invoke();
+    }
+
+    public void PauseGame()
+    {
+        isPaused = !isPaused;
+        Time.timeScale = isPaused ? 0 : 1;
+    }
+
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+        isPaused = false;
+        Time.timeScale = 1;
+    }
+
+    public void ReloadScene()
+    {
+        Time.timeScale = 1;
+        isPaused = false;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void SetDifficulty(DifficultyLevel newDifficulty)
+    {
+        currentDifficult = newDifficulty;
+        PlayerPrefs.SetInt("Difficulty", (int)newDifficulty); // Guarda la dificultad
+        PlayerPrefs.Save();
+    }
+}
---
+++ Assets/Scripts/UIControl.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIControl : MonoBehaviour
+{
+    public GameObject winScreen;
+    public GameObject loseScreen;
+    public GameObject PauseCanvas;
+
+    private void Start()
+    {
+        Debug.Log("Iniciando escena, asegurando Time.timeScale = 1"); // Para depuración
+        Time.timeScale = 1; // Asegura que el juego no inicia pausado
+        if (GameManager.GameInstance != null)
+        {
+            GameManager.GameInstance.isPaused = false; // Asegura que el estado no quede en pausa
+            GameManager.GameInstance.OnWin += Winner;
+            GameManager.GameInstance.OnLose += Loser;
+        }
+        PauseCanvas.SetActive(false); // Ocultar menú de pausa al inicio
+    }
+
+    private void OnDestroy()
+    {
+        // Evitar errores al cambiar de escena
+        if (GameManager.GameInstance != null)
+        {
+            GameManager.GameInstance.OnWin -= Winner;
+            GameManager.GameInstance.OnLose -= Loser;
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Pause"))
+        {
+            GameManager.GameInstance.PauseGame();
+            PauseCanvas.SetActive(GameManager.GameInstance.isPaused);
+        }
+    }
+
+    private void Winner()
+    {
+        winScreen.SetActive(true);
+        PauseCanvas.SetActive(false);
+    }
+
+    private void Loser()
+    {
+        loseScreen.SetActive(true);
+        PauseCanvas.SetActive(false);
+    }
+
+    public void LoadScene(string sceneName)
+    {
+        GameManager.GameInstance.LoadScene(sceneName);
+    }
+
+    public void ReloadScene()
+    {
+        GameManager.GameInstance.ReloadScene();
+        Time.timeScale = 1.0f;
+    }
+
+    public void ResumeGame()
+    {
+        GameManager.GameInstance.PauseGame();
+        PauseCanvas.SetActive(false);
+    }
+
+    public void SetDifficulty(int difficulty)
+    {
+        GameManager.GameInstance.SetDifficulty((DifficultyLevel)difficulty);
+    }
+}
